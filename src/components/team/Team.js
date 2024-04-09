d8fb83d1741ad699a362cdbf5286c74962e98cc8
MMarQs <108202305+MMarQs@users.noreply.github.com>
2024-04-09 10:29:50 +0100

    Team update

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -2,9 +2,11 @@ import React from 'react'
 import antonio_neves from '../static-images/antonio.jpeg'
 import marilia from '../static-images/marilia.jpg'
 import alexandre from '../static-images/alexandre.png'
+import bruno from '../static-images/bruno.jpg'
+import daniel from '../static-images/daniel.png'
+import diogo from '../static-images/diogo.png'
+import joao from '../static-images/joao.png'
 import miguel from '../static-images/miguel.png'
-
-import user1 from '../static-images/antonio.jpeg'
 
 import './Team.css'
 
@@ -31,21 +33,21 @@ const Team = () => {
                         <p>Developer</p>
                     </div>
                     <div className='card'>
-                        <img src={user1} alt='user1'/>
+                        <img src={bruno} alt='user1'/>
                         <p><span>Bruno Gomes</span></p>
                         <p>Developer</p>
                     </div>
                     <div className='card'>
-                        <img src={user1} alt='user1'/>
+                        <img src={daniel} alt='user1'/>
                         <p><span>Daniel Frank</span></p>
                         <p>Developer</p>
                     </div>
                     <div className='card'>
-                        <img src={user1} alt='user1'/>
+                        <img src={diogo} alt='user1'/>
                         <p><span>Diogo Silva</span></p>
                         <p>Developer</p>
                     </div>                    <div className='card'>
-                        <img src={user1} alt='user1'/>
+                        <img src={joao} alt='user1'/>
                         <p><span>João Gonçalves</span></p>
                         <p>Developer</p>
                     </div>
